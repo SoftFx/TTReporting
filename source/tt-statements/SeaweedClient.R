5aab25abd8fd794c97e0f78cccc0d1464751ae66
oksana.hilevich <oksana.hilevich@soft-fx.lv>
2026-08-25 19:15:22 +0300

    add buckets folder to Seaweed structure

--- source/tt-statements/SeaweedClient.R
+++ source/tt-statements/SeaweedClient.R
@@ -6,7 +6,7 @@ library(httr)
 # failure -- callers rely on their own tryCatch(..., error = function(e) FALSE) to turn a
 # thrown error into FALSE, same as a disk I/O error would.
 write_statement_to_seaweed <- function(html, domain, period_folder, login, filer_url) {
-  remote_path <- sprintf("%s/statements/%s/%s/%s_mail.html", filer_url, domain, period_folder, login)
+  remote_path <- sprintf("%s/buckets/statements/%s/%s/%s_mail.html", filer_url, domain, period_folder, login)
   resp <- PUT(remote_path, body = html)
   stop_for_status(resp)
   TRUE
